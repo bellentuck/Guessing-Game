f231b362f0368da649ad908a5ab2c347e73881e3
Ben Ellentuck <ellentuckben@gmail.com>
2018-02-24 23:21:10 -0500

    Refactoring code to improve DRYness and readability

--- index.js
+++ index.js
@@ -5,75 +5,75 @@
 
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  //makeCanvasFullPage();
-  //loadInGradients("https://github.com/ghosh/uiGradients/blob/master/gradients.json");
-  // setEventListeners();
-});
+//   //makeCanvasFullPage();
+//   //loadInGradients("https://github.com/ghosh/uiGradients/blob/master/gradients.json");
+//   // setEventListeners();
+// });
 
 
-  /* Guess Button animation */
-// function setEventListeners() {
-//   var goBtn = document.getElementById("submit");
-//   goBtn.addEventListener('click', function() {
-//     goBtn.preventDefault;
+//   /* Guess Button animation */
+// // function setEventListeners() {
+// //   var goBtn = document.getElementById("submit");
+// //   goBtn.addEventListener('click', function() {
+// //     goBtn.preventDefault;
+// //     // See: https://css-tricks.com/restart-css-animation/
+// //     goBtn.classList.remove("select-animation");
+// //     void goBtn.offsetWidth;
+// //     goBtn.classList.add("select-animation");
+// //   }, false);
+//   /*
+//   var input = document.getElementById("input-parent");
+//   input.addEventListener('mouseenter', function() {
+//     input.preventDefault;
 //     // See: https://css-tricks.com/restart-css-animation/
-//     goBtn.classList.remove("select-animation");
-//     void goBtn.offsetWidth;
-//     goBtn.classList.add("select-animation");
+//     console.log('here!');
+//     // $('#player-input').classList.remove("player-input-animation");
+//     // void $('#player-input').offsetWidth;
+//     // $('#player-input').classList.add("player-input-animation");
+//     // $('#submit').classList.remove("submit-input-animation");
+//     // void $('#submit').offsetWidth;
+//     // $('#submit').classList.add("submit-input-animation");
 //   }, false);
-  /*
-  var input = document.getElementById("input-parent");
-  input.addEventListener('mouseenter', function() {
-    input.preventDefault;
-    // See: https://css-tricks.com/restart-css-animation/
-    console.log('here!');
-    // $('#player-input').classList.remove("player-input-animation");
-    // void $('#player-input').offsetWidth;
-    // $('#player-input').classList.add("player-input-animation");
-    // $('#submit').classList.remove("submit-input-animation");
-    // void $('#submit').offsetWidth;
-    // $('#submit').classList.add("submit-input-animation");
-  }, false);
-  */
+//   */
+// // }
+
+
+
+// /* Gradient stuff */
+
+// function makeCanvasFullPage() {
+//   var canvas = document.getElementById('canvas');
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
 // }
 
+// function loadInGradients(url) {
+//   $.getJSON(url, function(json) {
+//     var results = getResultColors(json);
+//     setBackgroundGradient(results);
+//   });
+// }
 
+// function getResultColors(data) {
+//   var result = [];
+//   for (var i = 0; i < data.length; i++) {
+//     result = addColorsEntryToResults(result, data[i]['colors']);
+//   }
+//   return result;
+// }
 
-/* Gradient stuff */
+// function setBackgroundGradient(data) {
+//   var gradientInfo = data[getRandomInt(data.length)];
+//   var ctx = document.getElementById('canvas').getContext('2d');
+//   var gradient = ctx.createLinearGradient(0, 0, window.innerHeight, window.innerWidth);
+//   gradient.addColorStop(0, gradientInfo[0]);
+//   gradient.addColorStop(1, gradientInfo[1]);
+//   ctx.fillStyle = gradient;
+//   ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+// }
 
-function makeCanvasFullPage() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-function loadInGradients(url) {
-  $.getJSON(url, function(json) {
-    var results = getResultColors(json);
-    setBackgroundGradient(results);
-  });
-}
-
-function getResultColors(data) {
-  var result = [];
-  for (var i = 0; i < data.length; i++) {
-    result = addColorsEntryToResults(result, data[i]['colors']);
-  }
-  return result;
-}
-
-function setBackgroundGradient(data) {
-  var gradientInfo = data[getRandomInt(data.length)];
-  var ctx = document.getElementById('canvas').getContext('2d');
-  var gradient = ctx.createLinearGradient(0, 0, window.innerHeight, window.innerWidth);
-  gradient.addColorStop(0, gradientInfo[0]);
-  gradient.addColorStop(1, gradientInfo[1]);
-  ctx.fillStyle = gradient;
-  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -1,5 +1,73 @@
-// Game engine -------------------//
+/*
+1. Game Engine
+  a) Game object and prototype functions
+  b) Game helper functions
+2. View Logic
+  a) new game and reset
+  b) guess processing
+  c) game over
+  d) hints
+3. jQuery script
+*/
 
+/* ----------------------------- 1. Game Engine ----------------------------- */
+/* --------------- a) Game object and prototype functions ------------------- */
+function Game() {
+  this.playersGuess = null;
+  this.pastGuesses = [];
+  this.winningNumber = generateWinningNumber();
+  this.isOver = false;
+}
+
+Game.prototype.difference = function() {
+  return Math.abs(this.playersGuess - this.winningNumber);
+}
+
+Game.prototype.isLower = function() {
+  return this.playersGuess < this.winningNumber;
+}
+
+Game.prototype.playerMessages = {
+  win: "You Win!",
+  lose: "You Lose.",
+  close: "You're burning up!",
+  medium: "You're lukewarm.",
+  far: "You're a bit chilly.",
+  veryFar: "You're ice cold!",
+  duplicate: "You have already guessed that number."
+}
+
+Game.prototype.playersGuessSubmission = function(n) {
+  if (invalidGuess(n)) throw 'That is an invalid guess.';
+  this.playersGuess = n;
+  return this.checkGuess(); // have to actually *return* the result!
+}
+
+Game.prototype.checkGuess = function() {
+  if (this.pastGuesses.includes(this.playersGuess)) return this.playerMessages.duplicate;
+  this.pastGuesses.push(this.playersGuess);
+  if (this.playersGuess === this.winningNumber) return this.playerMessages.win;
+  if (this.pastGuesses.length === 5) return this.playerMessages.lose;
+  return this.getIntermediatePlayerMessage();
+}
+
+Game.prototype.getIntermediatePlayerMessage = function() {
+  let distFromWin = this.difference();
+  if (distFromWin < 50) {
+    if (distFromWin < 25) {
+      if (distFromWin < 10) return this.playerMessages.close;
+      return this.playerMessages.medium;
+    } return this.playerMessages.far;
+  } return this.playerMessages.veryFar;
+}
+
+Game.prototype.provideHint = function() {
+  return shuffle(
+    [this.winningNumber, generateWinningNumber(), generateWinningNumber()]
+  );
+}
+
+/* ---------------------- b) Game helper functions -------------------------- */
 function generateWinningNumber() {
   return Math.floor(Math.random() * 100 + 1);
 }
@@ -27,96 +95,92 @@ function newGame() {
 }
 
 
-function Game() {
-  this.playersGuess = null;
-  this.pastGuesses = [];
-  this.winningNumber = generateWinningNumber();
-  this.isOver = false;
+/* ----------------------------- 2. View Logic ------------------------------ */
+/* ------------------------ a) New game and reset --------------------------- */
+function hideGuessesList() {
+  $('#guesses-list li').hide();
+  $('#guessListTitle').hide();
+  $('#guesses').hide();
+}
+function hideAnswerLabel() {
+  $('.answerWas').hide();
 }
 
-Game.prototype.difference = function() {
-  return Math.abs(this.playersGuess - this.winningNumber);
+function resetTopLevelForRematch() {
+  resetHeaderForRematch();
+  $('.top-level').css("margin-top", "87px");
+  resetControlsForRematch();
 }
 
-Game.prototype.isLower = function() {
-  return this.playersGuess < this.winningNumber;
+function resetHeaderForRematch() {
+  $('#title').text('Guessing Game');
+  $('#subtitle').text('~owow it\'s a rematch~');
+  $('.header').removeClass('col-6 col-sm-3');
 }
 
-Game.prototype.playersGuessSubmission = function(n) {
-  if (invalidGuess(n)) throw 'That is an invalid guess.';
-  this.playersGuess = n;
-  return this.checkGuess(); // have to actually *return* the result!
+function resetControlsForRematch() {
+  var playerInput = document.getElementById('player-input');
+  var submitBtn = document.getElementById('submit');
+  playerInput.setAttribute("style", "z-index:1;");
+  submitBtn.setAttribute("style", "z-index:2;");
+  $('#player-input').attr('placeholder', '#');
+  document.getElementById('player-input').style.opacity = 1;
 }
 
-Game.prototype.playerMessages = {
-  win: "You Win!",
-  lose: "You Lose.",
-  close: "You're burning up!",
-  medium: "You're lukewarm.",
-  far: "You're a bit chilly.",
-  veryFar: "You're ice cold!",
-  duplicate: "You have already guessed that number."
+function enableSubmitAndHintButtons() {
+  $('#submit').prop("disabled", false);
+  $('#hint').prop("disabled", false);
 }
 
-Game.prototype.checkGuess = function() {
-
-  if (this.pastGuesses.includes(this.playersGuess)) return this.playerMessages.duplicate;
-
-  this.pastGuesses.push(this.playersGuess);
-
-  if (this.playersGuess === this.winningNumber) return this.playerMessages.win;
-  if (this.pastGuesses.length === 5) return this.playerMessages.lose;
-
-  var distFromWin = this.difference();
-  if (distFromWin < 50) {
-    if (distFromWin < 25) {
-      if (distFromWin < 10) return this.playerMessages.close;
-      return this.playerMessages.medium;
-    }
-    return this.playerMessages.far;
-  }
-  return this.playerMessages.veryFar;
-
+function runOpeningAnimations() {
+  $('#gameboard').removeClass('opening-animation-titles');
+  $('#input-container').removeClass('opening-animation-btns');
+  $('#footer').removeClass('opening-animation-footer');
+  void document.getElementById('gameboard').offsetWidth;
+  $('#gameboard').addClass('opening-animation-titles');
+  $('#input-container').addClass('opening-animation-btns');
+  $('#footer').addClass('opening-animation-footer');
 }
 
-Game.prototype.provideHint = function() {
-  return shuffle([this.winningNumber, generateWinningNumber(), generateWinningNumber()]);
-}
-
-
-// jQuery stuff ---------------------------//
+/* ------------------------ b) Guess Processing ----------------------------- */
 function processPlayerGuess(game) {
-
-
-  $('#submit').removeClass('select-animation');
-  void document.getElementById('submit').offsetWidth;
-  $('#submit').addClass('select-animation');
-
-  var guess = $('#player-input').val();
-  $('#player-input').val('');
-  var outcome = game.playersGuessSubmission(parseInt(guess, 10));
-
+  animateGoButton();
+  let outcome = getOutcomeFromGuess(game);
   document.getElementById('title').style.fontSize = "500%";
   processGuessOutcome(game, outcome);
 }
 
+function animateGoButton() {
+  $('#submit').removeClass('select-animation');
+  void document.getElementById('submit').offsetWidth;
+  $('#submit').addClass('select-animation');
+}
+
+function getOutcomeFromGuess(game) {
+  let guess = $('#player-input').val();
+  $('#player-input').val('');
+  return game.playersGuessSubmission(parseInt(guess, 10));
+}
+
+
+function showGuesses(game) {
+  $('.top-level').css("margin-top", "0px");
+  $('#guesses').slideDown(500);
+  $('#guessListTitle').show();
+  $('#guesses-list li:nth-child('+ game.pastGuesses.length +')').text(game.playersGuess).show(100);
+}
+
 function processGuessOutcome(game, outcome) {
-
   setTimeout(function(){
+    if (game.isOver) throw 'Cannot process guess if game is over';
 
-  if (game.isOver) throw 'Cannot process guess if game is over';
+    if (outcome === game.playerMessages.duplicate) {
+      // $('#title').text('Try a new number.');
+      document.getElementById('title').style.fontSize = "350%";
 
-  if (outcome === game.playerMessages.duplicate) {
-    $('#title').text('Try a new number.');
-    document.getElementById('title').style.fontSize = "350%";
-  } else {
-    $('.top-level').css("margin-top", "0px");
-    $('#guesses').slideDown(500);
-    $('#guessListTitle').show();
-    $('#guesses-list li:nth-child('+ game.pastGuesses.length +')').text(game.playersGuess).show(100);
-    //document.getElementById('footer').style.height = '80px';
-    // $('.header').addClass('col-6 col-sm-6').addClass('header-vertical');
-  }
+    } else {
+      showGuesses(game);
+    }
 
   if ((outcome === game.playerMessages.win) ||
      (outcome === game.playerMessages.lose)) {
@@ -125,35 +189,7 @@ function processGuessOutcome(game, outcome) {
     } else {
       $('#title').text('You lose.');
     }
-
-    // here's what happens when a game ends
-    // ( GAME OVER STUFF )
-    // (( here for now ))
-
-    $('#guessListTitle').hide();
-    $('#guesses-list li:nth-child(1)').text(game.playersGuess).hide(200);
-    $('#guesses-list li:nth-child(2)').text(game.playersGuess).hide(400);
-    $('#guesses-list li:nth-child(3)').text(game.playersGuess).hide(600);
-    $('#guesses-list li:nth-child(4)').text(game.playersGuess).hide(800);
-    $('#guesses-list li:nth-child(5)').text(game.playersGuess).hide(1000);
-    $('#guesses').hide(2000);
-
-    $('.top-level').css("margin-top", "87px");
-    //document.getElementById('footer').style.height = '200px';
-    $('#subtitle').text('~reset to rematch~');
-    $('#submit').prop("disabled", true);
-    $('#hint').prop("disabled", true);
-    $('.header').removeClass('col-6 col-sm-3');
-    game.isOver = true;
-    $('.answerWas').show();
-    // convert z-indexes to negative - which is actually perfect
-    // that way then you cant put in any guesses any more.
-    var playerInput = document.getElementById('player-input');
-    var submitBtn = document.getElementById('submit');
-    playerInput.setAttribute("style", "z-index:-2;");
-    submitBtn.setAttribute("style", "z-index:-1;");
-    $('#player-input').attr('placeholder', game.winningNumber);
-    document.getElementById('player-input').style.opacity = .9;
+    processEndgame(game);
 
   } else {
     var higherOrLower = game.isLower() ? 'higher' : 'lower';
@@ -164,13 +200,84 @@ function processGuessOutcome(game, outcome) {
   }, 200);
 }
 
-$(document).ready(function() {
-  var game = newGame();
-  $('#guesses-list li').hide();
-  $('.answerWas').hide();
-  $('#guessListTitle').hide();
-  $('#guesses').hide();
+/* --------------------------- c) Game Over --------------------------------- */
+function processEndgame(game) {
+  removeGuessList(game);
+  getTopLevelEndgameState();
+  $('#player-input').attr('placeholder', game.winningNumber);
+  game.isOver = true;
+}
 
+function removeGuessList(game) {
+  $('#guessListTitle').hide();
+  $('#guesses-list li:nth-child(1)').text(game.playersGuess).hide(200);
+  $('#guesses-list li:nth-child(2)').text(game.playersGuess).hide(400);
+  $('#guesses-list li:nth-child(3)').text(game.playersGuess).hide(600);
+  $('#guesses-list li:nth-child(4)').text(game.playersGuess).hide(800);
+  $('#guesses-list li:nth-child(5)').text(game.playersGuess).hide(1000);
+  $('#guesses').hide(2000);
+}
+
+function getTopLevelEndgameState() {
+  $('.top-level').css("margin-top", "87px");
+  displayResetMessage();
+  disableSubmitAndHintButtons();
+  $('.header').removeClass('col-6 col-sm-3');
+  displayAnswerOverGlossedInputAndSubmitOrbs();
+}
+
+function displayResetMessage() {
+  $('#subtitle').text('~reset to rematch~');
+}
+
+function disableSubmitAndHintButtons() {
+  $('#submit').prop("disabled", true);
+  $('#hint').prop("disabled", true);
+}
+
+function displayAnswerOverGlossedInputAndSubmitOrbs() {
+  $('.answerWas').show();
+  let playerInput = document.getElementById('player-input');
+  let submitBtn = document.getElementById('submit');
+  playerInput.setAttribute("style", "z-index:-2;");
+  submitBtn.setAttribute("style", "z-index:-1;");
+  document.getElementById('player-input').style.opacity = .9;
+}
+
+/* ------------------------------ d) Hints ---------------------------------- */
+function runHintTimer(hintTexts) {
+  let i = 1;
+  function hintTimer() {
+    $('#title').text(hintTexts[i]);
+    i++;
+    if (i === hintTexts.length) {
+      endHintTimer();
+    }
+  }
+  giveHint(hintTexts[0]);
+  let hintTimerInterval = setInterval(hintTimer, 4000);
+  document.getElementById('title').style.fontSize = "350%";
+}
+
+function endHintTimer() {
+  document.getElementById('title').style.fontSize = "500%";
+  $('#subtitle').text('Guess a number between 1 and 100...');
+  $('#title').removeClass('fade-thru');
+}
+
+function giveHint(hint) {
+  $('#title').addClass('fade-thru');
+  $('#title').text(hint);
+  $('#subtitle').text('');
+}
+
+
+/* ------------------------- 3. jQuery Script ------------------------------- */
+$(document).ready(function() {
+
+  var game = newGame();
+  hideGuessesList();
+  hideAnswerLabel();
 
   $('#submit').click(function(event) {
     processPlayerGuess(game);
@@ -182,8 +289,24 @@ $(document).ready(function() {
     }
   });
 
-  // $('#guesses-list').on('li', 'hover', function() {
-  //   var audio = document.getElementsByTagName("audio")[0];audio.play();
+  $('#hint').click(function() {
+    let hints = game.provideHint();
+    let hintTexts = ['First Hint: ' + hints[0], 'Second Hint: ' + hints[1], ' Third Hint: ' +     hints[2], 'Final Hint: you have now glimpsed the winning number...', 'Guessing Game']
+    runHintTimer(hintTexts);
+  });
+
+  $('#reset').click(function() {
+    game = newGame();
+    hideGuessesList();
+    hideAnswerLabel();
+    resetTopLevelForRematch();
+    enableSubmitAndHintButtons();
+    runOpeningAnimations();
+  });
+
+  //// For debugging purposes
+  // $('#title').on("DOMSubtreeModified",function(){
+  //   console.log('changed');
   // });
 
   var audio = $("#guessNostalgia")[0];
@@ -191,87 +314,4 @@ $(document).ready(function() {
     audio.play();
   });
 
-
-
-  // function hintTimer(hints) {
-  //     document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-  //     $('#title').text(hintText);
-  // }
-
-  $('#hint').click(function() {
-    var hints = game.provideHint();
-    var hintTexts = ['First Hint: ' + hints[0], 'Second Hint: ' + hints[1], ' Third Hint: ' +     hints[2], 'Final Hint: you have now glimpsed the winning number...', 'Guessing Game']
-    var i = 1;
-    function hintTimer() {
-      $('#title').text(hintTexts[i]);
-      i++;
-      if (i === hintTexts.length) {
-        document.getElementById('title').style.fontSize = "500%";
-        $('#subtitle').text('Guess a number between 1 and 100...');
-        $('#title').removeClass('fade-thru');
-      }
-    }
-    $('#title').addClass('fade-thru');
-    $('#title').text(hintTexts[0]);
-    $('#subtitle').text('');
-    var hintTimerInterval = setInterval(hintTimer, 4000);
-    // var hintText = 'Hint: the winning number is either ' + hints[0] + ', ' +
-    //   hints[1] + ', or ' + hints[2] + '.';
-    document.getElementById('title').style.fontSize = "350%";
-    // $('#title').text(hintText);
-  });
-
-  $('#reset').click(function() {
-    game = newGame();
-    $('#guesses-list li').hide();
-    $('.answerWas').hide();
-    $('#guessListTitle').hide();
-    $('#guesses').hide();
-    $('#title').text('Guessing Game');
-    $('#subtitle').text('~owow it\'s a rematch~');
-    $('.header').removeClass('col-6 col-sm-3');
-    //document.getElementById('footer').style.height = '200px';
-    $('.top-level').css("margin-top", "87px");
-    var playerInput = document.getElementById('player-input');
-    var submitBtn = document.getElementById('submit');
-    playerInput.setAttribute("style", "z-index:1;");
-    submitBtn.setAttribute("style", "z-index:2;");
-    $('#player-input').attr('placeholder', '#');
-    document.getElementById('player-input').style.opacity = 1;
-    $('#submit').prop("disabled", false);
-    $('#hint').prop("disabled", false);
-
-    $('#gameboard').removeClass('opening-animation-titles');
-    $('#input-container').removeClass('opening-animation-btns');
-    $('#footer').removeClass('opening-animation-footer');
-    void document.getElementById('gameboard').offsetWidth;
-    $('#gameboard').addClass('opening-animation-titles');
-    $('#input-container').addClass('opening-animation-btns');
-    $('#footer').addClass('opening-animation-footer');
-
-  });
-
-  $('#title').on("DOMSubtreeModified",function(){
-    //console.log('changed');
-  });
-
-
 });
-
-
-// Have a game-like obj but for gameboard
-
-// function newGame() {
-//   return new Game();
-// }
-
-
-// function Game() {
-//   this.playersGuess = null;
-//   this.pastGuesses = [];
-//   this.winningNumber = generateWinningNumber();
-// }
-
-// Game.prototype.difference = function() {
-//   return Math.abs(this.playersGuess - this.winningNumber);
-// }
